d344af21e4a851eee4444ab8368fda658f2fda68
Dan Beto <dan.beto31408@gmail.com>
2020-02-08 12:40:54 -0600

    Workshop Assignment 1: React-Native

---
+++ Components/AboutComponent.js
@@ -0,0 +1,63 @@
+import { ScrollView, FlatList, Text } from 'react-native';
+import { Component } from 'react';
+import { PARTNERS } from '../shared/partners.js';
+import { Card } from 'react-native-elements';
+
+function Mission() {
+    return (
+        <Card title="Our Mission">
+            <Text wrapperStyle={{ margin: 10 }}>
+                We present a curated database of the best campsites in the vast woods and backcountry
+                of the World Wide Web Wilderness. We increase
+                access to adventure for the public while
+                promoting safe and respectful use of resources.
+                The expert wilderness trekkers on our staff
+                personally verify each campsite to make sure that they are up to our standards.
+                We also present a platform for campers to share reviews on campsites they have visited with each other.
+                  </Text>
+        </Card>
+    );
+}
+
+
+
+class About extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        };
+    }
+
+    static navigationOptions = {
+        title: "About Us"
+    };
+
+    render() {
+        const renderPartner = ({ item }) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+                />
+            )
+        }
+        return (
+            <ScrollView>
+                <Mission />
+                <Card title="Community Partners">
+                    <FlatList
+                        data={this.state.partners}
+                        keyExtractor={item => item.description.toString()}
+                        renderItem={renderPartner}
+                    />
+                </Card>
+            </ScrollView>
+        )
+    };
+}
+
+
+export default About;
---
+++ Components/ContactComponent.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import { createStackNavigator, createDrawerNavigator } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: "Contact"
+    };
+
+    render() {
+        return (
+            <ScrollView>
+                <Card>
+                    <Text  style={{ margin: 20 }}>
+                Contact Information
+                    </Text>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seatle, WA 98001</Text>
+                    <Text  style={{ margin: 20 }}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text  style={{ margin: 10 }}>Email: campsites@nucamp.co</Text>
+                </Card>
+            </ScrollView>
+        );
+    };
+}
+
+const HomeNavigator = createStackNavigator(
+    {
+      Home: { screen: Home }
+    },
+    {
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: "#5637DD"
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          color: "#fff"
+        }
+      }
+    }
+  );
+
+  const HomeNavigator = createDrawerNavigator(
+    {
+      Home: { screen: HomeNavigator },
+      Directory: { screen: DirectoryNavigator }
+    },
+    {
+      drawerBackgroundColor: "#CEC8FF"
+    }
+  );
+
+
+export default Contact;
