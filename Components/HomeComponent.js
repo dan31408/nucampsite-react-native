e26c0ecfebc3830713ce16eb4ac7692906d842e5
Dan Beto <dan.beto31408@gmail.com>
2020-02-14 16:24:00 -0600

    Redux Adding Favorites

--- Components/HomeComponent.js
+++ Components/HomeComponent.js
@@ -49,19 +49,21 @@ class Home extends Component {
     return (
       <ScrollView>
         <RenderItem
-          item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+          item={this.props.campsites.campsites.filter(
+            campsite => campsite.featured)[0]}
           isLoading={this.props.campsites.isLoading}
           errMess={this.props.campsites.errMess}
         />
         <RenderItem
           item={
-            this.props.promotions.promotions.filter(promotion => promotion.featured)[0]
-          }
+            this.props.promotions.promotions.filter(
+              promotion => promotion.featured)[0]}
           isLoading={this.props.promotions.isLoading}
           errMess={this.props.promotions.errMess}
         />
         <RenderItem
-          item={this.props.partners.partners.filter(partner => partner.featured)[0]}
+          item={this.props.partners.partners.filter(
+            partner => partner.featured)[0]}
           isLoading={this.props.partners.isLoading}
           errMess={this.props.partners.errMess}
         />
